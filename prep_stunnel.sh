03cb6a2386b479599f5c33efc9731e0efafb4edf
Charles Reid <charlesreid1@gmail.com>
2017-04-06 10:07:08 +0000

    add mkdir commands to prep_stunnel script

--- prep_stunnel.sh
+++ prep_stunnel.sh
@@ -10,6 +10,8 @@ conf="stunnel.conf"
 if [ -e ${conf} ]; then
 	if [ "$(/bin/grep 'A.B.C.D' ${conf})" == "" ]; then
 		cp stunnel.conf /etc/stunnel/stunnel.conf
+		mkdir -p /var/run/stunnel4/
+		mkdir -p /var/log/stunnel4/
 	else
 		echo "Change A.B.C.D in ${conf} to the server's IP."
 	fi
